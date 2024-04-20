62e2427ae9df1424bce037d56697ac2514d7855d
Huong <huongnguyen.nth1709@gmail.com>
2024-04-20 20:35:49 +0300

    install react-router-dom and setup layout

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
-import './App.css';
+import { BrowserRouter, Navigate, Routes, Route } from 'react-router-dom';
+import HomePage from 'scenes/homepage';
+import LoginPage from 'scenes/login-page';
+import ProfilePage from 'scenes/profile-page';
 
 function App() {
-  return <div className='app'>Hello</div>;
+  return (
+    <div className='app'>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<LoginPage />}></Route>
+          <Route path='/home' element={<HomePage />}></Route>
+          <Route path='/profile/:userId' element={<ProfilePage />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
