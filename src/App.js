cd003f503ebfe29e4b9e161af34cb357797684b6
Huong <huongnguyen.nth1709@gmail.com>
2024-04-21 11:52:38 +0300

    create themeSetting according to light or dark mode and add it to app

--- src/App.js
+++ src/App.js
@@ -1,17 +1,30 @@
+import { useMemo } from 'react';
+import { useSelector } from 'react-redux';
 import { BrowserRouter, Navigate, Routes, Route } from 'react-router-dom';
+import { CssBaseline, ThemeProvider } from '@mui/material';
+import { createTheme } from '@mui/material/styles';
+
+import { themeSettings } from 'theme';
+
 import HomePage from 'scenes/homepage';
 import LoginPage from 'scenes/login-page';
 import ProfilePage from 'scenes/profile-page';
 
 function App() {
+  const mode = useSelector((state) => state.mode);
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
+
   return (
     <div className='app'>
       <BrowserRouter>
-        <Routes>
-          <Route path='/' element={<LoginPage />}></Route>
-          <Route path='/home' element={<HomePage />}></Route>
-          <Route path='/profile/:userId' element={<ProfilePage />}></Route>
-        </Routes>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Routes>
+            <Route path='/' element={<LoginPage />}></Route>
+            <Route path='/home' element={<HomePage />}></Route>
+            <Route path='/profile/:userId' element={<ProfilePage />}></Route>
+          </Routes>
+        </ThemeProvider>
       </BrowserRouter>
     </div>
   );
